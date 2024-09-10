1700a19bf1f1d9e2e51867c50a06f62d0127c2ad
Belen Cruz <vldr.bele5@gmail.com>
2024-09-10 12:28:40 -0600

    feat: funcion que maneja el input nombre

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 console.log ("Conteo de Vocales");
+
+/**
+ Encuentra el número de letras que eixten en el
+ nombre
+ @letra {string} letra a buscar
+ @nombre {string} nombre a buscar la letra
+ return número de letras encontradas
+*/
+function encontrarNumeroDeLetra( letra, nombre ) {    
+    let numeroDeLetras = 0;
+    for(let index = 0; index < nombre.length; index++ ) {
+        let letraAEvaluar = nombre.charAt( index );
+        if( letraAEvaluar === letra){
+            numeroDeLetras++;
+        }
+    }
+    return numeroDeLetras;
+}
+
+function manejoDelEventodeInputNombre(){
+    console.log("Me estoy invocando");
+}
+console.log(encontrarNumeroDeLetra("a", "estefany"));
